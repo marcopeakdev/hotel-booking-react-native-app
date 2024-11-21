9ccfbede243d096804c6b4146d7c89d7795f0c6c
marcopeakdev <peakgenius226@gmail.com>
2024-11-20 19:56:32 -0700

    configured the constans in react native app

---
+++ constants/date.ts
@@ -0,0 +1,25 @@
+export const MONTH_NAMES = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  
+  export const WEEK_DAY_NAMES = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
---
+++ constants/food.ts
@@ -0,0 +1,47 @@
+export const IN_STOCK_VALUE = "IN_STOCK";
+export const DINE_IN = "DINE_IN";
+export const TAKE_OUT = "TAKE_OUT";
+export const DINE_IN_NAME = "Dine In";
+export const TAKE_OUT_NAME = "Take Out";
+export const ADD_ON_ITEMS = [
+    {
+        name: "Bagels",
+        price: 10,
+        id: 1,
+    },
+    {
+        name: "Bagels1",
+        price: 10,
+        id: 2,
+    },
+    {
+        name: "Bagels2",
+        price: 10,
+        id: 3,
+    },
+    {
+        name: "Bagels3",
+        price: 10,
+        id: 4,
+    },
+    {
+        name: "Bagels4",
+        price: 10,
+        id: 5,
+    },
+    {
+        name: "Bagels5",
+        price: 10,
+        id: 6,
+    },
+    {
+        name: "Bagels6",
+        price: 10,
+        id: 7,
+    },
+    {
+        name: "Bagels7",
+        price: 10,
+        id: 8,
+    },
+];
